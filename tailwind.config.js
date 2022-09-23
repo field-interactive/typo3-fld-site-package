a718a46137af2c888b73fb0807687c1384eb359b
Sven Krefeld <info@svenkrefeld.de>
2022-09-23 13:12:15 +0200

    Add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./Resources/Private/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,8 @@ Encore
     //     pattern: /\.(png|jpg|jpeg)$/
     // })
 
+    .enablePostCssLoader()
+
     /*
      * ENTRY CONFIG
      *
